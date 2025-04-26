0f4e1b737a4dd24d6466f767cda54f3f8483a49c
MinkieYume <minkieyume@gmail.com>
2025-04-26 21:08:20 +0800

    dispatch更新

--- system/ui/menu_manager.gd
+++ system/ui/menu_manager.gd
@@ -4,9 +4,13 @@ extends LimboHSM
 @export var transition:Dictionary[StringName,LimboState]
 
 func _ready() -> void:
+	add_event_handler("return",_return)
 	for _menu in get_children():
 		if _menu is MenuState or _menu is MenuManager:
 			for event in transition.keys():
 				add_transition(_menu,transition[event],event)
 	initialize(self)
 	set_active(true)
+
+func _return(): # 返回到上级节点
+	return false
--- system/ui/menu_state.gd
+++ system/ui/menu_state.gd
@@ -4,6 +4,9 @@ extends LimboState
 @export var menu:Control
 @export var transition:Dictionary[StringName,LimboState]
 
+func _ready() -> void:
+	add_event_handler("return",_return)
+
 func _enter() -> void:
 	if is_instance_valid(menu):
 		menu.visible = true
@@ -13,3 +16,6 @@ func _exit() -> void:
 	if is_instance_valid(menu):
 		menu.visible = false
 		menu.release_focus()
+
+func _return(): # 返回到上级节点
+	return false
