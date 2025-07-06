f94449b5e83df6e6b011ca23f8d97c735c4ec78c
MinkieYume <git@yumieko.com>
2025-07-06 11:24:43 +0800

    阶段性优化

--- system/interactable_system/interactable.gd
+++ system/interactable_system/interactable.gd
@@ -1,5 +1,7 @@
 class_name Interactable
 extends Node2D
+## Interactable的接口类
+## 要实现该类，只需要实现该类的全部方法，并添加interactable的组即可。
 
 @export var id = "interactable1"
 
--- system/interactable_system/machine.gd
+++
@@ -1,5 +0,0 @@
-extends Interactable
-
-# 当机关被触发
-func active(character:String,handler:LevelHandler):
-	print("使用机关")
