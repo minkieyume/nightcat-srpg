d4b259505b3cc9e02b069d4af5299a2fd1b5c5e5
MinkieYume <git@yumieko.com>
2025-07-06 21:11:02 +0800

    优化视野扇形的渲染

--- system/grid_system/sight_view_server.gd
+++ system/grid_system/sight_view_server.gd
@@ -2,10 +2,10 @@ extends GridMapServer
 
 func _ready() -> void:
 	for character in level_handler.get_character_array():
-		character.update_sight_view(level_handler)
-		character.sight_updated.connect(_on_sight_updated)		
+		if !character.is_in_group("player"):
+			character.update_sight_view(level_handler)
+			character.sight_updated.connect(_on_sight_updated)
 
 func _on_sight_updated(id:String):
 	var character = level_handler.get_character(id)
-	if !character.is_in_group("player"):
-		character.update_sight_view(level_handler)
+	character.update_sight_view(level_handler)
