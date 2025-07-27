e73629ba3cb44730bde4c0250c9693d8f4a5bf5f
MinkieYume <sign@yumieko.com>
2025-07-27 11:13:31 +0800

    蜡笔重击

--- game/actions/action_logics/crayon_hit.gd
+++ game/actions/action_logics/crayon_hit.gd
@@ -5,19 +5,14 @@ func precheck(action:Action) -> bool:
 	var unit = LevelHandler.get_unit(quester.quest_unit(action.target))
 	if unit:			
 		if LevelHandler.is_unit_in_group(unit.id,"enemy"):
-			if !unit.has_buff("disarm"):
+			if unit.has_buff("trip") or unit.has_buff("fear"):
 				return true
-		else:
-			return true
 	return false
 
-# 蜡笔飞弹：破坏机关或者缴械敌人。
+# 蜡笔重击：对敌人进行处决一击
 func execute(action:Action):
 	var quester = LevelHandler.get_grid_quester()
 	var unit_id = quester.quest_unit(action.target)
-	var unit:Unit = LevelHandler.get_unit(unit_id)
-	if LevelHandler.is_unit_in_group(unit_id,"enemy"):
-		# 缴械效果，未实现
-		pass
-	elif LevelHandler.is_unit_in_group(unit_id,"breakable"):
-		unit.on_break()
+	var unit:Character = LevelHandler.get_unit(unit_id)
+	var damage:int = action.args["damage"]
+	unit.apply_damage(damage)
