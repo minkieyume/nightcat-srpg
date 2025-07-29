c4f8a6ff31f8dec058fe7bfc392b7142f7160434
MinkieYume <sign@yumieko.com>
2025-07-29 10:31:24 +0800

    fixgbug

--- game/phases/cat_feet_end.gd
+++ game/phases/cat_feet_end.gd
@@ -1,6 +1,6 @@
 extends Phase
 
-func _exit() -> void:
+func _enter() -> void:
 	var cats = LevelHandler.get_units().filter(func(u):return LevelHandler.is_unit_in_group(u.id,"cat_feet"))
 	for cat in cats:
 		cat.cat_feet = false
