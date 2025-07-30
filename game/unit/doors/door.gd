6e412d1e5afa7c1e05e552daa4e487c94e35bc1a
MinkieYume <sign@yumieko.com>
2025-07-30 15:47:50 +0800

    switch

--- game/unit/doors/door.gd
+++ game/unit/doors/door.gd
@@ -4,11 +4,11 @@ extends Interactable
 @onready var close_state = $AnimationMachine/Close
 
 func _init_animation_machine() -> void:
-	animation_machine.add_transition(open_state,close_state,"close")
-	animation_machine.add_transition(close_state,open_state,"open")
+	animation_machine.add_transition(open_state,close_state,"switch")
+	animation_machine.add_transition(close_state,open_state,"switch")
 	animation_machine.initialize(self)
 	animation_machine.set_active(true)
 
 @rpc("authority","call_local")
-func interact(_character:String,ctx:Dictionary):
-	pass
+func interact(_character:String,_ctx:Dictionary):
+	animation_machine.dispatch("switch")
--- game/unit/floors/floor.gd
+++ game/unit/floors/floor.gd
@@ -4,15 +4,19 @@ extends Unit
 @onready var smooth_state = $AnimationMachine/Smooth
 
 func _init_animation_machine() -> void:
-	animation_machine.add_transition(normal_state,smooth_state,"smooth")
-	animation_machine.add_transition(smooth_state,normal_state,"normal")
+	animation_machine.add_transition(normal_state,smooth_state,"switch")
+	animation_machine.add_transition(smooth_state,normal_state,"switch")
 	animation_machine.initialize(self)
 	animation_machine.set_active(true)
 
 @rpc("authority","call_local")
-func unit_enter(unit:Unit):
+func interact(_character:String,_ctx:Dictionary):
+	animation_machine.dispatch("switch")
+
+@rpc("authority","call_local")
+func unit_enter(_unit:Unit):
 	pass
 
 @rpc("authority","call_local")
-func unit_exit(unit:Unit):
+func unit_exit(_unit:Unit):
 	pass
